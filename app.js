cc978cc931b73847d6f096aadbc32e13da627b3b
Hossam <hossamhammam1@hotmail.com>
2021-09-01 17:58:31 +0200

    message

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ let s3 = new aws.S3({
 require("dotenv").config();
 // express app
 const app = express();
+
 // connect to mongodb & listen for requests
 dbURI = process.env.dbURI;
 console.log(dbURI);
@@ -27,7 +28,24 @@ app.use((req, res, next) => {
   res.locals.path = req.path;
   next();
 });
+//API
+var axios = require("axios").default;
 
+var options = {
+  method: 'GET',
+  url: 'https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/total',
+  params: {country: 'Canada'},
+  headers: {
+    'x-rapidapi-host': 'covid-19-coronavirus-statistics.p.rapidapi.com',
+    'x-rapidapi-key': '76cecf7d17msha3c3dc7ff3f8df6p115d0cjsn5de958d6c2e1'
+  }
+};
+let CT;
+axios.request(options).then(function (response) {
+	CT = response.data;
+}).catch(function (error) {
+	console.error(error);
+});
 // mongoose & mongo tests
 app.get('/add-blog', (req, res) => {
   const blog = new Blog({
@@ -76,6 +94,10 @@ app.get('/about', (req, res) => {
 // blog routes
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
+});
+
+app.get('/API', (req, res) => {
+  res.render('API', { title: 'API' , CT : CT});
 });
 
 app.get('/blogs', (req, res) => {
